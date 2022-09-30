f7f6061297f1a997eb7d89d4867a2f2997045b90
NickHuijgen01 <nkhuijgen@gmail.com>
2022-09-30 21:29:29 +0200

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ data.find(x => x.ID === 1).Perioden }}
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+import inflationData from '~/static/data.json'
+
+@Component
+export default class index extends Vue {
+  data: any[] = inflationData
+
+ mounted() {
+    console.log(this.getItemByDate('2022MM05'))
+ }
+
+ getItemByDate(period: string): object {
+    return this.data.find(object => object.Perioden === period)
+ }
+}
+</script>
+
+<style scoped>
+</style>
