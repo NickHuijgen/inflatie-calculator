7fb2dd642e13d4e03d03d42bd9a811bc2ea1c60b
NickHuijgen01 <nkhuijgen@gmail.com>
2022-10-01 12:03:08 +0200

    Simple input

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,40 @@
 <template>
   <div>
-    {{ output }}
+    <div class="h-screen flex justify-center items-center w-full">
+      <div class="bg-white px-10 py-8 bg-blue-200 rounded-xl w-screen shadow-md max-w-sm">
+        <div class="space-y-4">
+          <h1 class="text-center text-2xl font-semibold text-gray-600">Bereken inflatie</h1>
+          <div>
+            <label class="mb-1 text-gray-600 font-semibold">Aantal</label>
+            <input v-model="input" type="number" class="bg-gray-100 px-4 py-2 outline-none rounded-md w-full" />
+          </div>
+          <div>
+            <label class="block mb-1 text-gray-600 font-semibold">Jaar</label>
+            <input v-model="inputYear" type="number" class="bg-gray-100 px-4 py-2 outline-none rounded-md w-full" />
+          </div>
+
+          <div>
+            <label class="block mb-1 text-gray-600 font-semibold">Maand</label>
+            <select v-model="inputMonth" class="bg-gray-100 px-4 py-2 outline-none rounded-md w-full">
+              <option value="JJ00">Einde jaar</option>
+              <option value="MM01">Januari</option>
+              <option value="MM02">Februari</option>
+              <option value="MM03">Maart</option>
+              <option value="MM04">April</option>
+              <option value="MM05">Mei</option>
+              <option value="MM06">Juni</option>
+              <option value="MM07">Juli</option>
+              <option value="MM08">Augustus</option>
+              <option value="MM09">September</option>
+              <option value="MM10">Oktober</option>
+              <option value="MM11">November</option>
+              <option value="MM12">December</option>
+            </select>
+          </div>
+        </div>
+        <p class="mt-4 w-full py-2 rounded-md text-lg tracking-wide text-center">{{ input }}$ in {{ inputYear }} is {{ output }}$ in {{ outputYear }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -14,21 +48,25 @@ export default class index extends Vue {
 
   input: number = 100
   inputYear: string = '2005'
-  inputMonth: string = '00'
+  inputMonth: string = 'JJ00'
 
   yearOrMonth: string = 'JJ'
 
-  output: number = 0
   outputYear: string = '2021'
 
  mounted() {
-   this.output = this.calculateInflation(this.input, (this.inputYear + this.yearOrMonth + this.inputMonth), (this.outputYear + this.yearOrMonth + this.inputMonth))
+   // this.output = this.calculateInflation(this.input, (this.inputYear + this.yearOrMonth + this.inputMonth), (this.outputYear + this.yearOrMonth + this.inputMonth))
+ }
+
+ get output() {
+   return this.calculateInflation(this.input, (this.inputYear + this.inputMonth), (this.outputYear + this.inputMonth))
+
  }
 
  calculateInflation(value: number, period1: string, period2: string) {
     const yearMutationCpi = this.calculateYearMutationCPI(period1, period2)
 
-    return ((value * (yearMutationCpi / 100) * 100) / 100)
+    return (Math.round(value * (yearMutationCpi / 100) * 100) / 100)
  }
 
  calculateYearMutationCPI(period1: string, period2: string): number {
