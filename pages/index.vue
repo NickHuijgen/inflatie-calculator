8f49ca29189933fcca777d7883a2573f03f5c9a6
NickHuijgen01 <nkhuijgen@gmail.com>
2022-10-03 21:38:54 +0200

    styling

--- pages/index.vue
+++ pages/index.vue
@@ -24,8 +24,10 @@
               />
             </div>
 
-            <div @click="switchInputAndOutput">
-              switch
+            <div class="p-2 ml-4">
+              <svg @click="switchInputAndOutput" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5L7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
+              </svg>
             </div>
 
             <div class="p-2">
